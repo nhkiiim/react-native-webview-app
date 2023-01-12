3320565b54ea0d548a0509ecfb3ff6f80d22c352
nhkiiim <nhkiiim@naver.com>
2023-01-12 13:25:36 +0900

    Feat: RN 웹뷰 데이터 통신 추가

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useRef } from "react";
 import { WebView } from "react-native-webview";
+import { View, TouchableOpacity, Text, Alert } from "react-native";
 
-export default function App() {
-    return (
-       <WebView
-         source={{uri: 'https://github.com/facebook/react-native'}}
-       />
-    );
-}
+const WebViewScreen = ({ }) => {
+  //웹뷰에서 데이터 받기
+  const onMessage = (e) => {
+    console.log(e.nativeEvent.data);
+    Alert.alert(e.nativeEvent.data);
+  };
 
+  //RN에서 웹뷰로 데이터 보내기
+  const webViewRef = useRef()
+
+  const sendMessage = (e) => {
+    webViewRef.current.postMessage('RN에서 데이터 전송');
+  };
+
+  return (
+    <View style={{flex: 1}}>
+        <WebView
+            ref={webViewRef}
+            source={{uri: 'http://localhost:8480/webview'}}
+            onMessage={onMessage}
+        />
+        <View style={{flex: 1}}>
+            <TouchableOpacity onPress={sendMessage}>
+                <Text>RN에서 데이터 전송</Text>
+            </TouchableOpacity>
+        </View>
+    </View>
+  );
+};
+
+export default WebViewScreen;
